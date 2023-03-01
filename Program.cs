2773231b47d82b15110ae8653f8218d899e6172a
Chugunov <jw108mac@gmail.com>
2023-03-01 13:57:31 +0300

    Добавил решение задачи

--- Program.cs
+++ Program.cs
@@ -7,3 +7,20 @@
 ["1234","1567","-2","computer science"]->["-2"]
 ["Russia","Denmark","Kazan"]->[]
 */
+Console.Clear();
+Console.WriteLine("Введите число для сортировки массива");
+int sort = Convert.ToInt32(Console.ReadLine());
+string[] array = { "hello", "Russia", "Denmark", "-2", "computer science", "2", "world", ":-)", "GeekBrains","robo","home"};
+Console.Write($"[{String.Join(" , ", array)}] -> ");
+Console.Write("[ ");
+ModifyArray(sort,array);
+Console.WriteLine();
+
+void ModifyArray(int sort,string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= sort) Console.Write($"{arr[i]}" + ",");
+    }
+    Console.Write("\b ]");
+}
